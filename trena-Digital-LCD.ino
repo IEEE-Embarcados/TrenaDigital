43e6347fb2a5e6aa360295d39f220a0441eb2e1e
WandSilva <bilva.wanderson@gmail.com>
2018-06-26 18:31:00 -0300

    iniciando a implementaçao para alternar os modos usando uma chave

--- trena-Digital-LCD.ino
+++ trena-Digital-LCD.ino
@@ -3,14 +3,17 @@ LiquidCrystal lcd(3,4,11,10,9,8);
 
 #include <Ultrasonic.h>
 Ultrasonic ultrasonic(7,6); //define the name of sensor and set the pins -> trig(8) and echo(7) respectively
+
 int distance = 0;
 int readValue = 0;
-int btnPerimetroDist = 12;
-int btnAreaDist = 13;
-int btnPerimetroState = 0;
-int btnAreaState = 0;
 int perimetro = 0;
 int area = 1;
+
+int inputMode = 12;
+int btnReadDist = 13;
+int inputModeState=0;
+int btnReadState=0;
+
 void setup() {
   lcd.begin(20,4);
   lcd.setCursor(1,0);
@@ -24,16 +27,15 @@ void setup() {
   lcd.setCursor(3,3);
   lcd.print("Medida:");
 
+  pinMode(inputMode, INPUT);
+  pinMode(btnReadDist, INPUT_PULLUP);
 
-  pinMode(btnPerimetroDist, INPUT);
-  pinMode(btnAreaDist, INPUT);
   Serial.begin(9600); //serial comumnication 9600 bauds rates.
 }
 
 void loop() {
   readValue = ultrasonic.Ranging(CM);//return the distance in CM
 
-    distance = readValue;
   lcd.setCursor(15,3);
   lcd.print("    ");
   delay(10);
@@ -47,40 +49,45 @@ void loop() {
   lcd.print("cm");
 
   //Resolver a medida;
-  btnPerimetroState = digitalRead(btnPerimetroDist);
-  btnAreaState = digitalRead(btnAreaDist);
+  inputModeState = digitalRead(inputMode);
+  btnReadState = digitalRead(btnReadDist);
 
-  if(btnAreaState == HIGH){
+  if(btnReadState == HIGH){
     distance = readValue;
-    area = area * distance;
-    Serial.print(area);
-    Serial.println("CM"); 
 
-    lcd.setCursor(15,1);
-    lcd.print("    ");
-    delay(10);
+    if(inputModeState == HIGH){
+      distance = readValue;
+      area = area * distance;
+      Serial.print(area);
+      Serial.println("CM"); 
 
-    lcd.setCursor(14,1);
-    lcd.print(area);    
-    lcd.setCursor(17,1);
-    lcd.print("cm");
+      lcd.setCursor(15,1);
+      lcd.print("    ");
+      delay(1);
+
+      lcd.setCursor(14,1);
+      lcd.print(area);    
+      lcd.setCursor(17,1);
+      lcd.print("cm");
+    }
+
+    if(inputModeState == LOW){
+      perimetro = perimetro + distance;
+      Serial.print(perimetro);
+      Serial.println("CM");
+
+      lcd.setCursor(15,2);
+      lcd.print("    ");
+      delay(1);
+
+      lcd.setCursor(14,2);
+      lcd.print(perimetro);    
+      lcd.setCursor(17,2);
+      lcd.print("cm");    
+    }
+      delay(100);
   }
-
-  if(btnPerimetroState == HIGH){
-    perimetro = perimetro + distance;
-    Serial.print(perimetro);
-    Serial.println("CM");
-
-    lcd.setCursor(15,2);
-    lcd.print("    ");
-    delay(10);
-
-    lcd.setCursor(14,2);
-    lcd.print(perimetro);    
-    lcd.setCursor(17,2);
-    lcd.print("cm");    
-  }
-
-  delay(1000);
 }
+
+
 
