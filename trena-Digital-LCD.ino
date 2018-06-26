4d531ac95d9b1840f9f1813e5e75c7a1169aa00d
WandSilva <bilva.wanderson@gmail.com>
2018-06-26 17:57:19 -0300

    removendo temporariamente a conversão para metros

--- trena-Digital-LCD.ino
+++ trena-Digital-LCD.ino
@@ -32,41 +32,34 @@ void setup() {
 
 void loop() {
   readValue = ultrasonic.Ranging(CM);//return the distance in CM
- 
-  distance = readValue;
+
+    distance = readValue;
   lcd.setCursor(15,3);
   lcd.print("    ");
   delay(10);
-  
-  if(distance > 59){
-    lcd.setCursor(14,3);
-    Serial.print(distance);
-    Serial.print("\n");
-    lcd.print(distance/60);    
-    lcd.setCursor(17,3);
-    lcd.print("m");    
-  }else{  
-    lcd.setCursor(14,3);
-    Serial.print(distance);
-    Serial.print("\n");
-    lcd.print(distance);    
-    lcd.setCursor(17,3);
-    lcd.print("cm");
-  }
+
+
+  lcd.setCursor(14,3);
+  Serial.print(distance);
+  Serial.print("\n");
+  lcd.print(distance);    
+  lcd.setCursor(17,3);
+  lcd.print("cm");
+
   //Resolver a medida;
   btnPerimetroState = digitalRead(btnPerimetroDist);
   btnAreaState = digitalRead(btnAreaDist);
-    
+
   if(btnAreaState == HIGH){
     distance = readValue;
     area = area * distance;
     Serial.print(area);
     Serial.println("CM"); 
-    
+
     lcd.setCursor(15,1);
     lcd.print("    ");
     delay(10);
-    
+
     lcd.setCursor(14,1);
     lcd.print(area);    
     lcd.setCursor(17,1);
@@ -77,16 +70,17 @@ void loop() {
     perimetro = perimetro + distance;
     Serial.print(perimetro);
     Serial.println("CM");
-    
+
     lcd.setCursor(15,2);
     lcd.print("    ");
     delay(10);
-    
+
     lcd.setCursor(14,2);
     lcd.print(perimetro);    
     lcd.setCursor(17,2);
     lcd.print("cm");    
   }
-  
+
   delay(1000);
 }
+
